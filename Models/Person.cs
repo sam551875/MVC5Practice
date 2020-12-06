0fbdb5a64d1aa87dc79b3bb9da25afde65b6ca6e
sam551875 <sam551875@gmail.com>
2020-12-06 23:27:47 +0800

    Creat頁面欄位前端驗證(必填、輸入範圍)練習

--- Models/Person.cs
+++ Models/Person.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVC5Practice.Models
 {
     public class Person
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="名字欄位為必填！")]
         public string Name { get; set; }
+
+        [Required]
+        [Range(1,10, ErrorMessage = "請輸入年紀以確保小於10歲")]
         public int age { get; set; }
+    
     }
 }
