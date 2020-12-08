56a67327fbc8d1bbee1be00bfa29f247adcaa0ef
sam551875 <sam551875@gmail.com>
2020-12-08 23:03:14 +0800

    在 Model 上更改欄位名稱使各頁面統一，在 View 嘗試改寫單一頁面html

--- Models/Person.cs
+++ Models/Person.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace MVC5Practice.Models
 {
     public class Person
     {
+        [DisplayName("姓名")]
         public int Id { get; set; }
 
         [Required(ErrorMessageResourceName ="Person_Name_Required",ErrorMessageResourceType =typeof(ResourceFile.Resource1))]
