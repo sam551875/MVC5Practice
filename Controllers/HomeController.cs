3eb8a6825d654b918dfce37b8f6f032c654f81b9
sam551875 <sam551875@gmail.com>
2020-12-28 02:12:43 +0800

    MT103 Model 加入各欄位註解，判斷未登入直接導向到登入頁面

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,12 +11,26 @@ namespace MVC5Practice.Controllers
     {
         public ActionResult Index()
         {
+            if(isLogged() == false)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            ShowLogerInfo();
+
+            ViewBag.Title = "MT103電文合併平台";
             ViewBag.UserName = HttpContext.Session["UserName"].ToString();
             return View();
         }
 
         public ActionResult About()
         {
+            if (isLogged() == false)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            ShowLogerInfo();
+
             ViewBag.Message = "Your application description page.";
 
             return View();
@@ -24,6 +38,13 @@ namespace MVC5Practice.Controllers
 
         public ActionResult Contact()
         {
+            if (isLogged() == false)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            ShowLogerInfo();
+
             ViewBag.Message = "Your contact page.";
 
             return View();
@@ -39,6 +60,8 @@ namespace MVC5Practice.Controllers
         public ActionResult LogIn()
         {
             ViewBag.Message = "Sams LogIn.";
+
+            HideLogerInfo();
 
             return View();
         }
@@ -63,9 +86,72 @@ namespace MVC5Practice.Controllers
 
         public ActionResult TxnPage1()
         {
+            if (isLogged() == false)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            ShowLogerInfo();
+
             ViewBag.Message = "TxnPage1";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult TxnPage1(MT103Format model)
+        {
+            try
+            {
+                if(
+                    model.SendersReference == "" || model.TimeIndication == "" || model.BankOprationCode == "" || model.InstructionCode == "" || model.TransactionTypeCode == "" ||
+                    model.ValueDate_CurrencyCode_Amount == "" || model.Currency_InstructedAmount == "" || model.ExchangeRate == "" || model.OrderingCustomer == "" || model.SendingInstitution == "" ||
+                    model.OrderingInstitution == "" || model.SendersCorrespondent == "" || model.ReceiversCorrespondent == "" || model.ThirdReimbursementInstitution == "" || model.IntermediaryInstitution == "" ||
+                    model.AccountWithInstitution == "" || model.BeneficiaryCustomer == "" || model.RemittanceInformation == "" || model.DetailsofCharges == "" || model.SendersCharges == "" ||
+                    model.ReceiversCharges == "" || model.SendertoReceiverInformation == "" || model.RegulatoryReporting == ""
+                )
+                {
+                    return RedirectToAction("TxnPage1", "Home");
+                }
+                SetTxnSession("TxnPage1", model);
+                return RedirectToAction("TxnPage2", "Home", model);
+            }
+            catch
+            {
+                return View("ErrorPage");
+            }
+            
+        }
+
+
+        //--------------------------方法集暫時區塊------------------------------//
+        private bool isLogged()
+        {
+            if (HttpContext.Session["UserName"] is null || HttpContext.Session["UserName"].ToString() == "")
+            {
+                return false;
+            }
+            return true;
+        }
+        public void ShowLogerInfo()
+        {
+            ViewBag.UserName = HttpContext.Session["UserName"].ToString();
+            ViewBag.Welcome = "歡迎回來";
+        }
+        public void HideLogerInfo()
+        {
+            ViewBag.UserName = "";
+            ViewBag.Welcome = "";
+            HttpContext.Session.Remove("UserName");
+        }
+        private string _TxnSession = "Sam";
+        protected object GetTxnSession(string key)
+        {
+            return Session[_TxnSession + "_" + key];
+        }
+        protected void SetTxnSession(string key, object obj)
+        {
+            Session[_TxnSession + "_" + key] = obj;
         }
 
     }
